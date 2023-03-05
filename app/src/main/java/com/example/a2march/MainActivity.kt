b44e238e05cd169514f17b9b0869cd075ff2b60e
Macintoshi <macintoshi@Mari.local>
2023-03-05 22:01:05 +0400

    Initial commit

---
+++ app/src/main/java/com/example/a2march/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.example.a2march
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val pickDateBtn: Button = findViewById(R.id.PickaDateBtn)
+        val enterDate: TextView = findViewById(R.id.EnteredDate)
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
